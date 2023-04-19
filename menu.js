207d38d90bf0f589c95a6edc615b67cd0e06fea4
Techcolony <osabiola4real@gmail.com>
2023-04-19 18:43:08 +0100

    made changes to JS file and HTML file

--- menu.js
+++ menu.js
@@ -1,10 +1,28 @@
 const navMenu = document.getElementById('nav-menu');
 const toggleMenu = document.getElementById('toggle-menu');
 const closeMenu = document.getElementById('close-menu');
+const menuList = document.querySelectorAll('.nav-item');
 
-toggleMenu.addEventListener('click', () => {
-  navMenu.classList.toggle('show');
-});
-closeMenu.addEventListener('click', () => {
-  navMenu.classList.remove('show');
+function displayMenu() {
+  if (navMenu.style.display === 'none') {
+    navMenu.style.display = 'block';
+    toggleMenu.style.display = 'none';
+  } else {
+    navMenu.style.display = 'none';
+    toggleMenu.style.display = 'block';
+  }
+}
+
+function hideMenu() {
+  if (toggleMenu.style.display === 'none') {
+    toggleMenu.style.display = 'block';
+    navMenu.style.display = 'none';
+  } else {
+    navMenu.style.display = 'block';
+  }
+}
+toggleMenu.addEventListener('click', displayMenu);
+closeMenu.addEventListener('click', hideMenu);
+menuList.forEach((section) => {
+  section.addEventListener('click', hideMenu);
 });
