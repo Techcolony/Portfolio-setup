3689e6b99830aa700d1ae2e7d2e17b36d181048e
stino-x <107426247+stino-x@users.noreply.github.com>
2023-04-21 23:10:51 +0200

    implementing groundwork for popup project section

--- Pop-up.js
+++ Pop-up.js
@@ -33,63 +33,67 @@ const project = [
   },
 ];
 
-document.querySelectorAll('.button').forEach((work) => {
-  const id = work.getAttribute('id');
-  const popupCard = `
-    <div class="project" id="project">
-          <div class="popupTop">
-              <div class="projectPopupTitle">
-                <div class="titlePopup">
-                
-                  <h3 class="comp-title">${project[id].projectTitle}</h3>
-                  <img
-                  id="popupClose${id}"
-                  class="popupClose"
-                  src="./images/Icon - Cancel.svg"
-                  alt="close-btn"
-                  />
+const desktopMenu = () => {
+  document.querySelectorAll('.button').forEach((work) => {
+    const id = work.getAttribute('id');
+    const popupCard = `
+      <div class="project" id="project">
+            <div class="popupTop">
+                <div class="projectPopupTitle">
+                  <div class="titlePopup">
+                  
+                    <h3 class="comp-title">${project[id].projectTitle}</h3>
+                    <img
+                    id="popupClose${id}"
+                    class="popupClose"
+                    src="./images/Icon - Cancel.svg"
+                    alt="close-btn"
+                    />
+                  </div>
+                  <ul class="work-title">
+                        <li class="canopy">CANOPY</li>
+                        <li><img src="icons-images/Counter.png" alt="counter" /></li>
+                        <li class="role">Back End Dev</li>
+                        <li><img src="icons-images/Counter.png" alt="counter" /></li>
+                        <li class="role">2015</li>
+                    </ul>
                 </div>
-                <ul class="work-title">
-                      <li class="canopy">CANOPY</li>
-                      <li><img src="icons-images/Counter.png" alt="counter" /></li>
-                      <li class="role">Back End Dev</li>
-                      <li><img src="icons-images/Counter.png" alt="counter" /></li>
-                      <li class="role">2015</li>
+                <img class="projectImage" src=${project[id].imageURL} alt="project-image">
+              <div class="popupDown">
+              <div class="project-desc popupDesc">${project[id].description}</div>
+                <div class="popupBtns">
+                  <ul class="tags">
+                    <li class="tag">${project[id].technologies[0]}</li>
+                    <li class="tag">${project[id].technologies[1]}</li>
+                    <li class="tag">${project[id].technologies[2]}</li>
+                    <li class="tag">${project[id].technologies[3]}</li>
                   </ul>
-              </div>
-              <img class="projectImage" src=${project[id].imageURL} alt="project-image">
-            <div class="popupDown">
-            <div class="project-desc popupDesc">${project[id].description}</div>
-              <div class="popupBtns">
-                <ul class="tags">
-                  <li class="tag">${project[id].technologies[0]}</li>
-                  <li class="tag">${project[id].technologies[1]}</li>
-                  <li class="tag">${project[id].technologies[2]}</li>
-                  <li class="tag">${project[id].technologies[3]}</li>
-                </ul>
-                <hr>
-                <div class="actionBtnPopup">
-                <a class="btn btnPopup" href=${project[id].seeLive} target='blank'>
-                See Live <img class="seeAll" src='/icons/Iconlive.svg' alt="seeLive"></a>
-                <a class="btn btnPopup" href=${project[id].sourceCode} target='blank'>
-                See Source <img class="seeAll" src='/icons/Groupsource.svg' alt="githubCode"></a>
+                  <hr>
+                  <div class="actionBtnPopup">
+                  <a class="btn btnPopup" href=${project[id].seeLive} target='blank'>
+                  See Live <img class="seeAll" src='/icons/Iconlive.svg' alt="seeLive"></a>
+                  <a class="btn btnPopup" href=${project[id].sourceCode} target='blank'>
+                  See Source <img class="seeAll" src='/icons/Groupsource.svg' alt="githubCode"></a>
+                  </div>
                 </div>
               </div>
-            </div>
-            </div>
-        </div>
-      </div>`;
-    work.addEventListener('click', () => {
-      const cardWorks = document.getElementById('cardWorks');
-      const cards = document.createElement('cards');
-      cards.innerHTML = popupCard;
-      cards.classList.add('hide');
-      cardWorks.appendChild(cards);
-      cards.classList.add('projectPopup');
-      if (cards.classList.contains('hide')) { cards.classList.remove('hide'); }
-      const hidePopup = document.getElementById(`popupClose${id}`);
-      hidePopup.addEventListener('click', () => {
-        cardWorks.removeChild(cards);
-      });
+              </div>
+          </div>
+        </div>`;
+      // work.addEventListener('click', () => {
+      //   const cardWorks = document.getElementById('cardWorks');
+      //   const cards = document.createElement('cards');
+      //   cards.innerHTML = popupCard;
+      //   cards.classList.add('hide');
+      //   cardWorks.appendChild(cards);
+      //   cards.classList.add('projectPopup');
+      //   if (cards.classList.contains('hide')) { cards.classList.remove('hide'); }
+      //   const hidePopup = document.getElementById(`popupClose${id}`);
+      //   hidePopup.addEventListener('click', () => {
+      //     cardWorks.removeChild(cards);
+      //   });
+      // });
+      // projects.innerHTML = popupCard;
     });
-  });
+}
+document.querySelectorAll('.button').addEventListener('click', desktopMenu)
